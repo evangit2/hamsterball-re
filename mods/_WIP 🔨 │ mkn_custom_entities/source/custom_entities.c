7b4891aaf64824b0eacdd307a4bf319643983c28
evangit2 <105747602+evangit2@users.noreply.github.com>
2026-07-23 00:20:21 +0000

    v53f: Fix Wobbly wobble - add objects to Scene_Update list (board+0x8B8)
    Root cause: The board's Scene_Update function iterates board+0x8B8
    to call vtable[1] (Update) on each object. The mod was only adding
    objects to board+0x2578 (a different list for dynamic objects tracking).
    Objects in board+0x2578 never got their vtable[1] called, so
    Rotator_Update (which deforms vertices = the wobble) never ran.
    
    Fix: Added BOARD_SCENE_UPDATE_LIST (0x8B8) and append objects to
    both board+0x2578 AND board+0x8B8. Also updated despawn to remove
    from both lists.
    
    This should enable the wobble animation for Wobbly entities,
    and may also fix behaviour for other entities that rely on
    vtable[1] being called per-frame.

--- mods/_WIP 🔨 │ mkn_custom_entities/source/custom_entities.c
+++ mods/_WIP 🔨 │ mkn_custom_entities/source/custom_entities.c
@@ -99,6 +99,7 @@ static SpatialTree_Cleanup_t pfn_SpatialTree_Cleanup = (SpatialTree_Cleanup_t)0x
 #define BOARD_LEVEL             0x8AC
 #define BOARD_UPDATE_LIST       0x2578
 #define BOARD_RENDER_LIST       0xCD4
+#define BOARD_SCENE_UPDATE_LIST 0x8B8  /* Scene_Update iterates this list, calls vtable[1] */
 #define BOARD_COLLISION_LIST   0x10EC
 #define BOARD_SCENE_OBJ         0x8B0
 
@@ -995,6 +996,8 @@ static void cEnt_spawn_testcube_at(DWORD board, float px, float py, float pz, in
 
     /* 5. Add to board+0x2578 (update list) */
     pfn_AthenaList_Append((DWORD*)(board + BOARD_UPDATE_LIST), obj);
+    /* 5a. Also add to board+0x8B8 (Scene_Update list) for vtable[1] calls */
+    pfn_AthenaList_Append((DWORD*)(board + BOARD_SCENE_UPDATE_LIST), obj);
 
     /* 6. Add to board+0xCD4 (render list) */
     pfn_AthenaList_Append((DWORD*)(board + BOARD_RENDER_LIST), obj);
@@ -1083,6 +1086,7 @@ static void cEnt_despawn_object(DWORD board, DWORD obj, FILE* logf) {
 
     /* 3. Remove obj from board+0x2578 (update list) */
     pfn_AthenaList_Remove((DWORD*)(board + BOARD_UPDATE_LIST), (int)obj);
+    pfn_AthenaList_Remove((DWORD*)(board + BOARD_SCENE_UPDATE_LIST), (int)obj);
 
     /* 4. Remove obj from board+0xCD4 (render list) */
     pfn_AthenaList_Remove((DWORD*)(board + BOARD_RENDER_LIST), (int)obj);
@@ -1392,6 +1396,7 @@ static void cEnt_spawn_rotater_at(DWORD board, float px, float py, float pz,
 
         /* Register on board lists */
         pfn_AthenaList_Append((DWORD*)(board + BOARD_UPDATE_LIST), obj);
+        pfn_AthenaList_Append((DWORD*)(board + BOARD_SCENE_UPDATE_LIST), obj);
         pfn_AthenaList_Append((DWORD*)(board + BOARD_RENDER_LIST), obj);
 
         /* Add collision object */
@@ -1715,6 +1720,7 @@ static void cEnt_spawn_rotater_at(DWORD board, float px, float py, float pz,
 
                     /* Register on board lists */
                     pfn_AthenaList_Append((DWORD*)(board + BOARD_UPDATE_LIST), obj);
+                    pfn_AthenaList_Append((DWORD*)(board + BOARD_SCENE_UPDATE_LIST), obj);
                     pfn_AthenaList_Append((DWORD*)(board + BOARD_RENDER_LIST), render_obj);
 
                     /* Track in bridgeslam state array */
@@ -1798,6 +1804,8 @@ static void cEnt_spawn_rotater_at(DWORD board, float px, float py, float pz,
 
     /* 5. Add to board+0x2578 (update list) */
     pfn_AthenaList_Append((DWORD*)(board + BOARD_UPDATE_LIST), obj);
+    /* 5a. Also add to board+0x8B8 (Scene_Update list) for vtable[1] calls */
+    pfn_AthenaList_Append((DWORD*)(board + BOARD_SCENE_UPDATE_LIST), obj);
 
     /* 5b. BadBall also goes into the bad balls list (board+0x29D4) */
     if (ai_type == 15) {
